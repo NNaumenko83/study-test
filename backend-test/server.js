48093f7f09386bdcab732d084e49613e4e8ba3a0
Mykola <nnv2006@ukr.net>
2024-04-16 13:52:32 +0300

    ADD: cors

--- backend-test/server.js
+++ backend-test/server.js
@@ -3,8 +3,13 @@ require("dotenv").config();
 require("colors");
 const moviesRouter = require("./moviesRoutes/moviesRouter");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use((req, res, next) => {
     console.log("First middleware");
